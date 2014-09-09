1187b957722f36901562dfab32cb9fd75bed973f
Tommaso Madonia <tommaso@madonia.me>
2014-09-09 16:33:56 +0200

    Creata classe Voce

---
+++ WIP/Prototipo/DynamiCal/DynamiCal/Model/Voce.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DynamiCal.Model
+{
+    class Voce
+    {
+        private string _nome;
+
+        public Voce() : this(null) {}
+        public Voce(String nome) {
+            _nome = nome;
+        }
+
+        public string nome
+        {
+            set
+            {
+                _nome = value;
+            }
+            get
+            {
+                return _nome;
+            }
+        }
+    }
+}
