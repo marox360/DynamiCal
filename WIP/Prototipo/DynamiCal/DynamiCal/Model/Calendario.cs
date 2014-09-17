6b79ee020a04e4b0ae02c067eec7f6a82f7c7545
Tommaso Madonia <tommaso@madonia.me>
2014-09-17 15:00:17 +0200

    Aggiunto Calendario

---
+++ WIP/Prototipo/DynamiCal/DynamiCal/Model/Calendario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DynamiCal.Model
+{
+    class Calendario
+    {
+        private string _nome;
+
+        public Calendario(string nome)
+        {
+            #region Precondizioni
+            Debug.Assert(!String.IsNullOrWhiteSpace(nome), "Nome is null or white space");
+            #endregion
+
+            _nome = nome;
+        }
+
+        public string nome
+        {
+            get
+            {
+                return _nome;
+            }
+        }
+    }
+}
