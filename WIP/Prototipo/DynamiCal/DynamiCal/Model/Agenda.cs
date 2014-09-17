5bd42d4ca8924cd130a9da465b294dfee884c52c
Tommaso Madonia <tommaso@madonia.me>
2014-09-17 16:36:46 +0200

    Update Agenda

--- WIP/Prototipo/DynamiCal/DynamiCal/Model/Agenda.cs
+++ WIP/Prototipo/DynamiCal/DynamiCal/Model/Agenda.cs
@@ -8,9 +8,29 @@ namespace DynamiCal.Model
 {
     class Agenda
     {
+        private List<Calendario> _calendari;
+        private List<Evento> _eventi;
+
         public Agenda()
         {
+            _calendari = new List<Calendario>();
+            _eventi = new List<Evento>();
+        }
 
+        public List<Calendario> calendari
+        {
+            get
+            {
+                return _calendari;
+            }
+        }
+
+        public List<Evento> eventi
+        {
+            get
+            {
+                return _eventi;
+            }
         }
     }
 }
