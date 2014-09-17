d25e5202a2703107864cfe7988fbc2818ca27705
marox360 <lmarox@gmail.com>
2014-09-17 17:06:01 +0200

    Modifiche Evento

--- WIP/Prototipo/DynamiCal/DynamiCal/Model/Evento.cs
+++ WIP/Prototipo/DynamiCal/DynamiCal/Model/Evento.cs
@@ -8,11 +8,15 @@ namespace DynamiCal.Model
 {
     class Evento
     {
-        private string _nome, _descrizione;
+        private string _nome, _descrizione, _luogo;
         private DateTime _data;
         private long _durata;
+        private Periodicita _periodicita;
+        private ModelloEvento _modello;
 
-
-
+        public Evento(string nome, DateTime data, long durata, string descrizione, Periodicita periodicita, ModelloEvento modello, string luogo)
+        {
+ 
+        }
     }
 }
