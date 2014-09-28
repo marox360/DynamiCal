cd6023c31491516bcc5df3bb9874c8e7d738db70
Tommaso Madonia <tommaso@madonia.me>
2014-09-28 23:45:42 +0200

    Implementazione Periodicità

--- WIP/Prototipo/DynamiCal/DynamiCal/Model/Periodicita.cs
+++ WIP/Prototipo/DynamiCal/DynamiCal/Model/Periodicita.cs
@@ -7,9 +7,65 @@ namespace DynamiCal.Model
 {
     class Periodicita
     {
-        public Periodicita()
-        {
+        private int _ogniGiorno;
+        private int _ogniSettimana;
+        private int _ogniMese;
+        private int _ogniAnno;
 
+        public Periodicita(int ogniGiorno, int ogniSettimana, int ogniMese, int ogniAnno)
+        {
+            _ogniGiorno = ogniGiorno;
+            _ogniSettimana = ogniSettimana;
+            _ogniMese = ogniMese;
+            _ogniAnno = ogniAnno;
+        }
+
+        public int OgniGiorno
+        {
+            set
+            {
+                _ogniGiorno = value;
+            }
+            get
+            {
+                return _ogniGiorno;
+            }
+        }
+
+        public int OgniSettimana
+        {
+            set
+            {
+                _ogniSettimana = value;
+            }
+            get
+            {
+                return _ogniSettimana;
+            }
+        }
+
+        public int OgniMese
+        {
+            set
+            {
+                _ogniMese = value;
+            }
+            get
+            {
+                return _ogniMese;
+            }
+        }
+
+        public int OgniAnno
+        {
+            set
+            {
+                _ogniAnno = value;
+            }
+            get
+            {
+                return _ogniAnno;
+            }
         }
     }
 }
