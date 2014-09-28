c9ce4950bafa3516707d764bf82fc05312de74d2
Tommaso Madonia <tommaso@madonia.me>
2014-09-28 23:56:46 +0200

    Implementazione Evento

--- WIP/Prototipo/DynamiCal/DynamiCal/Model/Evento.cs
+++ WIP/Prototipo/DynamiCal/DynamiCal/Model/Evento.cs
@@ -34,5 +34,97 @@ namespace DynamiCal.Model
             _periodicita = periodicita;
             _modello = modello;
         }
+
+        public string Nome
+        {
+            set
+            {
+                #region Precondizioni
+                Debug.Assert(!String.IsNullOrWhiteSpace(value), "Nome is null or whitespace");
+                #endregion
+
+                _nome = value;
+            }
+            get
+            {
+                return _nome;
+            }
+        }
+
+        public string Descrizione
+        {
+            set
+            {
+                _descrizione = value;
+            }
+            get
+            {
+                return _descrizione;
+            }
+        }
+
+        public string Luogo
+        {
+            set
+            {
+                _luogo = value;
+            }
+            get
+            {
+                return _luogo;
+            }
+        }
+
+        public DateTime Data
+        {
+            set
+            {
+                #region Precondizioni
+                Debug.Assert(value != null, "Data is null");
+                #endregion
+
+                _data = value;
+            }
+            get
+            {
+                return _data;
+            }
+        }
+
+        public long Durata
+        {
+            set
+            {
+                #region Precondizioni
+                Debug.Assert(value < 0, "Durata is negative");
+                #endregion
+
+                _durata = value;
+            }
+            get
+            {
+                return _durata;
+            }
+        }
+
+        public Periodicita Periodicita
+        {
+            set
+            {
+                _periodicita = value;
+            }
+            get
+            {
+                return _periodicita;
+            }
+        }
+
+        public ModelloEvento Modello
+        {
+            get
+            {
+                return _modello;
+            }
+        }
     }
 }
