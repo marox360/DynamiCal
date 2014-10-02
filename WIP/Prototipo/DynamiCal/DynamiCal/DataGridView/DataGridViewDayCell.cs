e16cf4899ea88e1a459d21b13a4dfdfd2af38f36
Tommaso Madonia <tommaso@madonia.me>
2014-10-02 18:53:35 +0200

    Corretta la visualizzazione dei mesi su 5 settimane

--- WIP/Prototipo/DynamiCal/DynamiCal/DataGridView/DataGridViewDayCell.cs
+++ WIP/Prototipo/DynamiCal/DynamiCal/DataGridView/DataGridViewDayCell.cs
@@ -19,7 +19,7 @@ namespace DynamiCal.DataGridView
             {
                 cellStyle.BackColor = Color.FromArgb(242, 242, 242);
             }
-            if ((rowIndex == 0 && (value as CalendarDay).Date.Day > 7) || (rowIndex == 5 && (value as CalendarDay).Date.Day < 8))
+            if ((rowIndex == 0 && (value as CalendarDay).Date.Day > 7) || (rowIndex >= 4 && (value as CalendarDay).Date.Day < 15))
             {
                 cellStyle.ForeColor = Color.Gray;
             }
