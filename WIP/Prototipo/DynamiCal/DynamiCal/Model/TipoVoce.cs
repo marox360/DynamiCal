7b9139e0e72b3f35af1af41d8fa3e978fbff56bc
Tommaso Madonia <tommaso@madonia.me>
2014-10-02 18:44:58 +0200

    Creata una classe per TipoVoce

---
+++ WIP/Prototipo/DynamiCal/DynamiCal/Model/TipoVoce.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace DynamiCal.Model
+{
+    class TipoVoce
+    {
+        public enum Type
+        {
+            [Description("Testo")]
+            Stringa,
+            [Description("Numero")]
+            Double,
+            [Description("Sì/No")]
+            Boolean,
+            [Description("Data")]
+            Data
+        };
+
+        private Type _tipo;
+
+        public TipoVoce(Type tipo)
+        {
+            _tipo = tipo;
+        }
+
+        public Type Tipo
+        {
+            get
+            {
+                return _tipo;
+            }
+        }
+
+        public string Descrizione
+        {
+            get
+            {
+                return this.GetEnumDescription((Type)_tipo);
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.Descrizione;
+        }
+
+        public static List<TipoVoce> GetAllTypes()
+        {
+            List<TipoVoce> allTypes = new List<TipoVoce>();
+            foreach (Type tipo in Enum.GetValues(typeof(Type)).Cast<Type>())
+            {
+                allTypes.Add(new TipoVoce(tipo));
+            }
+
+            return allTypes;
+        }
+
+        private string GetEnumDescription(Enum value)
+        {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+            DescriptionAttribute attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+    }
+}
--- WIP/Prototipo/DynamiCal/DynamiCal/Model/Voce.cs
+++ WIP/Prototipo/DynamiCal/DynamiCal/Model/Voce.cs
@@ -4,29 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
-using System.ComponentModel;
 
 namespace DynamiCal.Model
 {
     class Voce
     {
-        public enum TipoVoce {
-            [Description("Testo")]
-            Stringa,
-            [Description("Numero")]
-            Double,
-            [Description("Sì/No")]
-            Boolean,
-            [Description("Data")]
-            Data};
-
         private string _nome;
         private TipoVoce _tipo;
 
         public Voce(string nome, TipoVoce tipo) 
         {
             #region Precondizioni
-            Debug.Assert(!String.IsNullOrWhiteSpace(nome), "Event Name is null");
+            Debug.Assert(!String.IsNullOrWhiteSpace(nome), "Nome is null or white space");
+            Debug.Assert(tipo != null, "Tipo is null");
             #endregion
 
             _nome = nome;
