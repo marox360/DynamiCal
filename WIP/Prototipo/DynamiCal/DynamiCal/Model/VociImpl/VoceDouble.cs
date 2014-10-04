8e6d9128fd7a1de6869fb2a0ec99661aff883b23
marox360 <lmarox@gmail.com>
2014-10-04 12:36:00 +0200

    Create le voci specializzate + la factory

---
+++ WIP/Prototipo/DynamiCal/DynamiCal/Model/VociImpl/VoceDouble.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DynamiCal.Model
+{
+    class VoceDouble : Voce
+    {
+        private string _nome;
+        private TipoVoce _tipo;
+        private double _valore;
+
+        public VoceDouble(string nome, TipoVoce tipo) : base(nome, tipo)
+        {
+            _valore = 0;
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+        }
+
+        public TipoVoce Tipo
+        {
+            get
+            {
+                return _tipo;
+            }
+        }
+
+        public double Valore
+        {
+            get
+            {
+                return _valore;
+            }
+            set
+            {
+                this._valore = value;
+            }
+        }
+    }
+}
