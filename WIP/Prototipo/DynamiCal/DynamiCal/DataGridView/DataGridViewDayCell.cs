853dcd8d79dc57f6b887b1d9c4a3afa53057176f
Tommaso Madonia <tommaso@madonia.me>
2014-10-04 17:03:50 +0200

    Ops, const non si può usare

--- WIP/Prototipo/DynamiCal/DynamiCal/DataGridView/DataGridViewDayCell.cs
+++ WIP/Prototipo/DynamiCal/DynamiCal/DataGridView/DataGridViewDayCell.cs
@@ -13,9 +13,9 @@ namespace DynamiCal.DataGridView
 {
     public class DataGridViewDayCell : DataGridViewTextBoxCell
     {
-        private const Color TodayColor = Color.FromArgb(192, 85, 87);
-        private const Color ThisWeekColor = Color.FromArgb(244, 203, 204);
-        private const Color WeekSeparatorColor = Color.FromArgb(214, 211, 211);
+        private static readonly Color TodayColor = Color.FromArgb(192, 85, 87);
+        private static readonly Color ThisWeekColor = Color.FromArgb(244, 203, 204);
+        private static readonly Color WeekSeparatorColor = Color.FromArgb(214, 211, 211);
 
         public DataGridViewDayCell() : base() { }
 
